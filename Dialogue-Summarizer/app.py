9d93d7cb82f1475e1f5c4e439dff0f9e2134c2f5
MyriamBA <mariam.benahmed@supcom.tn>
2025-04-04 14:15:46 +0200

    Optimized Inference Speed

--- Dialogue-Summarizer/app.py
+++ Dialogue-Summarizer/app.py
@@ -3,8 +3,10 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
 from peft import PeftModel
 import torch 
 
-# Load the fine-tuned model and tokenizer
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.benchmark = True 
 
+# Load the fine-tuned model and tokenizer
 peft_model_base = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-large", torch_dtype=torch.bfloat16)
 tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-large")
 
@@ -16,25 +18,29 @@ peft_model = PeftModel.from_pretrained(peft_model_base,
                                        is_trainable=False)
 
 # Move Model to GPU if available
-device = torch.device("cpu")  # Force CPU
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 peft_model.to(device)
 
+
+# Compile the model for further speedup
+peft_model = torch.compile(peft_model) 
+
 def summarize_dialogue(dialogue):
-    inputs = tokenizer(dialogue, return_tensors="pt", max_length=(512), truncation=True, padding="max_length")
-    inputs = {k: v.to(device) for k, v in inputs.items()}
+    with torch.inference_mode():  
+
+      inputs = tokenizer(dialogue, return_tensors="pt", max_length=(512), truncation=True, padding="max_length")
+      inputs = {k: v.to(device) for k, v in inputs.items()}
 
 
 
-
-    summary_ids=peft_model.generate(input_ids= inputs["input_ids"], generation_config=GenerationConfig(max_new_tokens=150, num_beams=1))
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+      summary_ids=peft_model.generate(input_ids= inputs["input_ids"], generation_config=GenerationConfig(max_new_tokens=100, num_beams=1, do_sample=False ))
+      summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
     return summary
 
 # Streamlit App
-st.title("Dialogue Summarizer with PEFT")
-st.write("Enter a dialogue below to get a summary powered by a fine-tuned PEFT model!:")
+st.title("Dialogue Summarizer")
+st.write("Enter a dialogue below to get a summary!:")
 
 #Input Dialogue
 dialogue_input = st.text_area("Enter Dialogue:", height=200, placeholder="Paste your dialogue here...")
